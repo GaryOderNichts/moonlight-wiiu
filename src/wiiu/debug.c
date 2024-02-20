78ba0b31877cfc96ecdb1811a4dac7262b0e18b2
GaryOderNichts <12049776+GaryOderNichts@users.noreply.github.com>
2024-02-20 01:36:08 +0100

    Log with OSReport on debug builds

--- src/wiiu/debug.c
+++ src/wiiu/debug.c
@@ -1,31 +1,21 @@
 #ifdef DEBUG
 #include <sys/iosupport.h>
-#include <whb/proc.h>
-#include <whb/crash.h>
-#include <whb/log.h>
-#include <whb/log_udp.h>
+#include <coreinit/debug.h>
 
 #include <stdlib.h>
 #include <stdio.h>
 
 static ssize_t wiiu_log_write(struct _reent* r, void* fd, const char* ptr, size_t len) {
-  WHBLogWritef("%*.*s", len, len, ptr);
+  OSReport("%*.*s", len, len, ptr);
   return len;
 }
+
 static const devoptab_t dotab_stdout = {
-  .name = "stdout_whb",
+  .name = "stdout_console",
   .write_r = wiiu_log_write,
 };
 
-void __wrap_abort() {
-  WHBLogPrint("Abort called! Forcing stack trace.");
-  *(unsigned int*)(0xDEADC0DE) = 0xBABECAFE;
-  for(;;) {}
-}
-
-void Debug_Init() {
-  WHBInitCrashHandler();
-  WHBLogUdpInit();
+void Debug_Init(){
   devoptab_list[STD_OUT] = &dotab_stdout;
   devoptab_list[STD_ERR] = &dotab_stdout;
 }
